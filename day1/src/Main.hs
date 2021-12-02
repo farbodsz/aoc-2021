9ee4905b6b1d884564b343425fbb8abc77f18b04
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-12-02 13:42:29 +0000

    Add Day 1 Part B

--- day1/src/Main.hs
+++ day1/src/Main.hs
@@ -1,9 +1,18 @@
 module Main where
 
 main :: IO ()
-main = interact $ show . solve . map read . words
+main = interact $ show . solve' . map read . words
 
+-- Part 1
 solve :: [Int] -> Int
-solve []             = 0
-solve [x           ] = 0
 solve (x1 : x2 : xs) = (if x1 < x2 then 1 else 0) + solve (x2 : xs)
+solve _              = 0
+
+-- Part 2
+solve' :: [Int] -> Int
+solve' = solve . map sumWindow . mkWindows
+    where sumWindow (x1, x2, x3) = x1 + x2 + x3
+
+mkWindows :: [Int] -> [(Int, Int, Int)]
+mkWindows (x1 : x2 : x3 : xs) = (x1, x2, x3) : mkWindows (x2 : x3 : xs)
+mkWindows _                   = []

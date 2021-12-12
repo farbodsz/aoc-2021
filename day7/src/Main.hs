89327cf2205fc6d71352148018351d8e687ded7a
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2021-12-12 11:31:29 +0000

    Complete Day 7

--- day7/src/Main.hs
+++ day7/src/Main.hs
@@ -3,8 +3,17 @@ module Main where
 import           Data.List.Split
 
 main :: IO ()
-main = interact $ show . solve . map read . splitOn ","
+main = interact $ show . solve' . map read . splitOn ","
 
 solve :: [Int] -> Int
-solve xs = minimum $ map totalDelta xs
-    where totalDelta target = sum $ map (abs . subtract target) xs
+solve xs = minimum $ map totalDelta range
+  where
+    range = [minimum xs .. maximum xs]
+    totalDelta target = sum $ map (abs . subtract target) xs
+
+solve' :: [Int] -> Int
+solve' xs = minimum $ map totalDelta range
+  where
+    range = [minimum xs .. maximum xs]
+    totalDelta target = sum $ map (fuelCost . abs . subtract target) xs
+    fuelCost dx = (dx * (dx + 1)) `div` 2
